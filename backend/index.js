3743a80b928d06afc0e7b160a3e0b5e89f76929a
Nivm200 <Nivm200@gmail.com>
2021-09-24 20:16:18 +0300

    Added request JSON validator
    Validating JSON requests, and allow passing through of request with a certain JSON format only.

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,38 @@
-var express = require("express");
-var app = express();
-var port = 3001;
+require("dotenv").config;
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
 
-app.listen(port, function () {
-  console.log("app started");
+const port = process.env.PORT || 3001;
+
+app.use(bodyParser.json());
+
+//Json validator middleware
+app.use((err, req, res, next) => {
+  // 400 if the json is in error
+  if (err.status === 400) return res.status(err.status).send("Request is not in JSON format.");
+  return next(err); // if it's not a 400, let the default error handling do it.
 });
 
 app.get("/", function (req, res) {
   res.send("hello world!");
+});
+
+app.post("/data/:type", async (req, res) => {
+  let body = req.body;
+  if (body) {
+    let { database, sensor, reading } = body;
+    if (database && sensor && reading) {
+      res.send({ success: "reading data" });
+      return;
+    }
+    res.send({ error: "Request body does not match the format." });
+    return;
+  }
+  res.send({ error: "Error reading data. Does the request body follows the format?" });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+  console.log(process.env.PORT);
 });
